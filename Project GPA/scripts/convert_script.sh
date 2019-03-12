6d349ca30cf32360855b8f64e15f1139defb10df
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2019-03-11 21:43:08 -0400

    Add files via upload

---
+++ Project GPA/scripts/convert_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#sbatch -N 1
+#sbatch -p RM
+#sbatch -t 02:00:00
+
+#./convert maps/gh001.el maps/gh001.totem -d
+#./convert maps/gh002.el maps/gh002.totem -u
+#./convert maps/gh003.el maps/gh003.totem -d
+#./convert maps/gh004.el maps/gh004.totem -d
+#./convert maps/gh005.el maps/gh005.totem -d
+#./convert maps/gh006.el maps/gh006.totem -u
+#./convert maps/gh007.el maps/gh007.totem -d
+#./convert maps/gh008.el maps/gh008.totem -u
+#./convert maps/gh009.el maps/gh009.totem -u
+#./convert maps/gh010.el maps/gh010.totem -u
+#./convert maps/gh011.el maps/gh011.totem -u
+#./convert maps/gh012.el maps/gh012.totem -u
+#./convert maps/gh013.el maps/gh013.totem -d
+#./convert maps/gh014.el maps/gh014.totem -d
+#./convert maps/gh015.el maps/gh015.totem -d
+./convert maps/gh016.el maps/gh016.totem -d 
+
+exit
